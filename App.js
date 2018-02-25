facacab2777ed3d256e362b35f24c05746dd9bac
hao.cong <hao.cong@app2m.com>
2018-02-25 20:02:23 +0800

    modify project config

--- App.js
+++ App.js
@@ -6,10 +6,7 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+  Platform, StyleSheet, Text, View
 } from 'react-native';
 
 const instructions = Platform.select({
@@ -19,8 +16,9 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-export default class App extends Component<{}> {
+export default class App extends Component {
   render() {
+    console.log('aaaaaaaaaa')
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
